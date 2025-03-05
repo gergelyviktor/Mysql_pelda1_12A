c03ba935c61563e5b8c09128babfd46f54658782
gergelyviktor <gergelyv@kkszki.hu>
2025-03-05 10:40:24 +0100

    Add project files.

---
+++ Mysql_pelda1_12A/Konyv.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mysql_pelda1_12A {
+    internal class Konyv {
+        public int Id { get; set; }
+        public string Cim { get; set; }
+        public int SzerzoId { get; set; }
+        public int Helyezes { get; set; }
+        public Konyv(int id, string cim, int szerzoId, int helyezes) {
+            Id = id;
+            Cim = cim;
+            SzerzoId = szerzoId;
+            Helyezes = helyezes;
+        }
+    }
+}
---
+++ Mysql_pelda1_12A/Szerzo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mysql_pelda1_12A {
+    internal class Szerzo {
+        public int Id { get; set; }
+        public string Nev { get; set; }
+        public int SzulEv { get; set; }
+        public int HalEv { get; set; }
+        public string Nemzetiseg { get; set; }
+    }
+}
